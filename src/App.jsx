2ceb766793d8461a52819475bdde6bafe344bce3
Emmanuel James <chiderajames55@gmail.com>
2023-06-12 18:41:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ import locationIcon from "./assets/locationIcon.png";
 function App() {
     const [newsData, setNewsData] = useState([]);
     const apiUrl =
-        "https://gnews.io/api/v4/search?q=technology&lang=en&max=10&apikey=f9442b6ce8092c601193f0eda3b8c24d";
-    // const apiKey = "f9442b6ce8092c601193f0eda3b8c24d";
+        "https://gnews.io/api/v4/search?q=technology&lang=en&max=10&apikey=";
+    const apiKey = "f9442b6ce8092c601193f0eda3b8c24d";
+    // const [publishedAt, setPublishedAt] = useState({
+    //     data: "",
+    //     time: "",
+    // });
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch(apiUrl);
+            const response = await fetch(apiUrl + apiKey);
             const data = await response.json();
             setNewsData([...data.articles]);
         };
@@ -19,38 +23,46 @@ function App() {
         fetchData();
     }, []);
 
+    // const publishedAtData = newsData.map((article) => {
+    //     set
+    // });
+    let date;
+    let time;
+
     const newsDataElement = newsData.map((article, index) => (
-        <section className="news-section">
-            <div className="news-img">
-                <img src={image} alt="" className="img" />
-            </div>
-            <div className="news-details" key={index}>
+        <section className="news-section" key={index}>
+            <figure className="news-img">
+                <img src={article.image} alt="" className="img" />
+            </figure>
+            <section className="news-details" key={index}>
                 <div className="location">
                     <img src={locationIcon} alt="" />
                     <h4>NIGERIA</h4>
-                    <a href="google.com">View on Source</a>
+                    <a
+                        href={article.source.url}
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        View on {article.source.name}
+                    </a>
                 </div>
                 <h1>{article.title}</h1>
                 <div className="publish-date">
-                    <h5>12 Jan, 2021 - 24 Jan, 2021</h5>
-                    <h5>10:20amWAT</h5>
+                    {[date, time] = article.publishedAt.split('T')}
+                    <h5>{date}</h5>
+                    <h5>{time}</h5>
                 </div>
-                <p>
-                    Mount Fuji is the tallest mountain in Japan, standing at
-                    3,776 meters (12,380 feet). Mount Fuji is the single most
-                    popular tourist site in Japan, for both Japanese and foreign
-                    tourists.
-                </p>
-            </div>
+                <p>{article.description}</p>
+            </section>
         </section>
     ));
 
     return (
         <div className="app-container">
-            <header className="header">
+            <header>
                 <h1>NewsApp</h1>
             </header>
-            {newsDataElement}
+            <main>{newsDataElement}</main>
         </div>
     );
 }
