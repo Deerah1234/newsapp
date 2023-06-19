581012e4056735a59d3a481027f6138215ef0bd4
Emmanuel James <chiderajames55@gmail.com>
2023-06-19 16:04:06 +0100

    Update configuration and add roadmap
    - Modify src/utils/config.js
    - Add roadmap.txt

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,9 @@
-export async function getNews() {
-    // const apiUrl =
-    //     "https://gnews.io/api/v4/search?q=technology&lang=en&max=10&apikey=";
-    const apiUrl =
-        "https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=";
-    // const apiKey = "f9442b6ce8092c601193f0eda3b8c24d";
-    const apiKey = "cbcda5c90da24fa09c59e11cec3010a2";
+import apiKey from "./config";
 
-    const res = await fetch(apiUrl + apiKey);
+export async function getNews() {
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=${apiKey}`;
+
+    const res = await fetch(apiUrl);
     if (!res.ok) {
         throw {
             message: "Failed to fetch News",
