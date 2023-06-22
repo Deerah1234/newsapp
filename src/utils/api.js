cc4bad6c18f4c4f4f62df62eab3abb1664ea206a
Emmanuel James <chiderajames55@gmail.com>
2023-06-22 13:30:33 +0100

    fix: some bugs

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,19 @@
 export async function getNews(id) {
-    // const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=`;
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=`;
 
-    // const apiKey = "cbcda5c90da24fa09c59e11cec3010a2";
+    const apiKey = "cbcda5c90da24fa09c59e11cec3010a2";
 
-    const apiKey = process.env.API_KEY;
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=${apiKey}`;
+    // const apiKey = process.env.API_KEY;
+    // const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=${apiKey}`;
 
     const res = await fetch(apiUrl + apiKey);
-    // if (!res.ok) {
-    //     throw {
-    //         message: "Failed to fetch News",
-    //         statusText: res.statusText,
-    //         status: res.status,
-    //     };
-    // }
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch News",
+            statusText: res.statusText,
+            status: res.status,
+        };
+    }
     const data = await res.json();
     return id ? data.articles[id] : data.articles;
 }
