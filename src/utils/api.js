f7002f7ae4d8128ffb86fc50c4f0017837ea87d2
Emmanuel James <chiderajames55@gmail.com>
2023-06-22 13:20:09 +0100

    add: process.env.API_KEY

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,10 @@
 export async function getNews(id) {
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=`;
+    // const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=`;
 
-    const apiKey = "cbcda5c90da24fa09c59e11cec3010a2";
+    // const apiKey = "cbcda5c90da24fa09c59e11cec3010a2";
+
+    const apiKey = process.env.API_KEY;
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=${apiKey}`;
 
     const res = await fetch(apiUrl + apiKey);
     if (!res.ok) {
