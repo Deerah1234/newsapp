42e2aada464f73e29a645a068af6dc5004ce71ce
Emmanuel James <chiderajames55@gmail.com>
2023-06-26 16:56:05 +0100

    add: add apikey & comment out base

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,7 @@
 export async function getNews(id) {
-    const apiKey = import.meta.env.VITE_API_KEY;
+    // const apiKey = import.meta.env.VITE_API_KEY;
+    // I will change the apiKey soon.
+    const apiKey = "cbcda5c90da24fa09c59e11cec3010a2";
     const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&max=10&apiKey=${apiKey}`;
 
     const res = await fetch(apiUrl);
