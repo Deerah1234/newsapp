8cc65ce575e2cf98357222f7fcea319711b311f1
Emmanuel James <chiderajames55@gmail.com>
2023-06-29 04:56:54 +0100

    add: defered data

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,20 +1,20 @@
-// import { useState, useEffect } from "react";
+import { Suspense } from "react";
 import fallbackImage from "../assets/nasa-unsplash.jpg";
 import CategoryIcon from "@mui/icons-material/Category";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
 import TodayIcon from "@mui/icons-material/Today";
-import { useLoaderData, Link } from "react-router-dom";
+import { useLoaderData, Link, defer, Await } from "react-router-dom";
 import { getNews } from "../utils/api";
 
 import "animate.css";
 
-export async function loader() {
-    return getNews();
+export function loader() {
+    return defer({ newsData: getNews() });
 }
 
 const News = () => {
-    const newsData = useLoaderData();
+    const newsDataPromise = useLoaderData();
 
     const formatDate = (time) => {
         const options = { year: "numeric", month: "long", day: "numeric" };
@@ -30,60 +30,79 @@ const News = () => {
         return formattedTime;
     };
 
-    const newsDataElement = newsData.map((article, index) => (
-        <section key={index} className="news-section">
-            <figure className="news-img">
-                {article.urlToImage ? (
-                    <>
-                        <img src={article.urlToImage} alt="" className="img" />
-                        <figcaption>Source: {article.source.name}</figcaption>
-                    </>
-                ) : (
-                    <>
-                        <img src={fallbackImage} alt="" className="img" />
-                        <figcaption>Source: unsplash-@Nasa</figcaption>
-                    </>
-                )}
-            </figure>
-            <section className="news-details" key={index}>
-                <div className="location">
-                    <CategoryIcon />
-                    <h4>TECHNOLOGY</h4>
-                    <a
-                        href={article.url}
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        View on {article.source.name}
-                    </a>
-                </div>
-                <h1>{article.title}</h1>
-                <div className="publish-date">
-                    {article.publishedAt && (
-                        <section>
-                            <h5 className="date">
-                                <TodayIcon />
-                                {formatDate(article.publishedAt)}
-                            </h5>
-                            <h5 className="time">
-                                {" - "}
-                                <AccessTimeFilledIcon />
-                                {formatTime(article.publishedAt)}
-                                {" UTC "}
-                            </h5>
-                        </section>
+    function renderNewsElements(newsData) {
+        const newsDataElement = newsData.map((article, index) => (
+            <section key={index} className="news-section">
+                <figure className="news-img">
+                    {article.urlToImage ? (
+                        <>
+                            <img
+                                src={article.urlToImage}
+                                alt=""
+                                className="img"
+                            />
+                            <figcaption>
+                                Source: {article.source.name}
+                            </figcaption>
+                        </>
+                    ) : (
+                        <>
+                            <img src={fallbackImage} alt="" className="img" />
+                            <figcaption>Source: unsplash-@Nasa</figcaption>
+                        </>
                     )}
-                </div>
-                <p className="news-description">{article.description}</p>
-                <Link className="news-story" to={`news/${index}`}>
-                    <VisibilityOffIcon />
-                    Click to view full story
-                </Link>
+                </figure>
+                <section className="news-details" key={index}>
+                    <div className="location">
+                        <CategoryIcon />
+                        <h4>TECHNOLOGY</h4>
+                        <a
+                            href={article.url}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            View on {article.source.name}
+                        </a>
+                    </div>
+                    <h1>{article.title}</h1>
+                    <div className="publish-date">
+                        {article.publishedAt && (
+                            <section>
+                                <h5 className="date">
+                                    <TodayIcon />
+                                    {formatDate(article.publishedAt)}
+                                </h5>
+                                <h5 className="time">
+                                    {" - "}
+                                    <AccessTimeFilledIcon />
+                                    {formatTime(article.publishedAt)}
+                                    {" UTC "}
+                                </h5>
+                            </section>
+                        )}
+                    </div>
+                    <p className="news-description">{article.description}</p>
+                    <Link className="news-story" to={`news/${index}`}>
+                        <VisibilityOffIcon />
+                        Click to view full story
+                    </Link>
+                </section>
             </section>
-        </section>
-    ));
+        ));
+        return (
+            <>
+                <div>{newsDataElement}</div>
+            </>
+        );
+    }
 
-    return <div>{newsDataElement}</div>;
+    return (
+        <Suspense fallback={<h1>Loading News...</h1>}>
+            <Await resolve={newsDataPromise.newsData}>
+                {renderNewsElements}
+            </Await>
+        </Suspense>
+    );
 };
 
 export default News;
